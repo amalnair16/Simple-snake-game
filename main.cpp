b231a1658cb8b39925e541c9c33499f33fa677df
Amal Nair <amal.nair16@gmail.com>
2021-12-29 20:31:59 +0000

    Declared start point in setup fuction

--- main.cpp
+++ main.cpp
@@ -1,13 +1,25 @@
 #include <iostream>
 using namespace std;
 bool gameOver;
+const int width =20;
+const int height=20;
+int x,y,fruitX,fruitY,score;
+enum eDirection {STOP=0,LEFT,RIGHT,UP,DOWN};
+eDirection dir;
 void setup()
 {
   gameOver=false;
+  dir =STOP;
+  x=width/2;
+  y=height/2;
+  fruitX=rand() %width;
+  fruitY=rand() %height;
 
 }
 void draw()
 {
+  system("cls");
+  
 
 
 }
