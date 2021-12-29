2f305055b8fd4910102e7fa9dcb41ebe8bc735f6
Amal Nair <amal.nair16@gmail.com>
2021-12-29 20:15:02 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+bool gameOver;
+void setup()
+{
+  gameOver=false;
+
+}
+void draw()
+{
+
+
+}
+void input()
+{
+
+}
+void logic()
+{
+
+}
+
+int main() {
+  setup();
+  while (!gameOver)
+  {
+    draw();
+    input();
+    logic();
+  }
+} 
